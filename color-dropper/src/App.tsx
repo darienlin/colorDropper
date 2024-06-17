d1715dfb07b4fe7077a0e90a3084a2ecda090ecc
Darien Lin <darienlin87@gmail.com>
2024-06-17 03:39:30 -0700

    added a button to the extension where if u click it it makes ur mouse turn into a dropper

--- color-dropper/src/App.tsx
+++ color-dropper/src/App.tsx
@@ -8,10 +8,29 @@ const toggleCursor = () => {
   setCursorActive(!cursorActive)
 }
 
+const clickMe = async () =>{
+  let [tab] = await chrome.tabs.query({active: true});
+
+  chrome.scripting.insertCSS({
+    target: {tabId: tab.id!},
+    files: ['App.css']
+  })
+
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id!},
+    func: () => {
+      document.body.classList.add("cursor");
+      //console logs it so we can use it
+      console.log(chrome.runtime.getURL('dropper16.png'))
+    }
+  })
+}
+
 
   return (
     <div className={cursorActive ? 'app cursor': 'app'}>
       <div>Color Dropper</div>
+      <button onClick={clickMe}>Click here to alert</button>
       <button onClick={toggleCursor}>Click here to Color Drop</button>
     </div>
   );
